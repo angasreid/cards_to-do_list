d05955de8ee9b664089871f1bf8066dc1c054ed2
Angas <angasreid@gmail.com>
2014-10-23 12:01:58 -0400

    Added cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # get 'projects/index'
 
   # get 'project/:id', to: 'projects#show', as: "<project></project>"
-  resources :projects, except: [:new, :edit]
-
+  resources :projects, except: [:new, :edit] do
+  resources :cards, except: [:new, :edit, :show, :index]
+	end
   devise_for :users
-
-   root 'home#index'
+  root 'home#index'
 
   
 end
